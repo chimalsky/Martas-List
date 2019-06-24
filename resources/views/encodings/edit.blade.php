0058de50aacf03094607d2beb9e3686f7245f902
chimalsky <ich.bin.abe@gmail.com>
2019-06-24 08:38:36 -0400

    even more up

--- resources/views/encodings/edit.blade.php
+++ resources/views/encodings/edit.blade.php
@@ -14,21 +14,9 @@
     {{ html()->form()->close() }}
 </header>
 
-<section class="flex flex-wrap mb-8 p-4 border border-1 border-gray-600">
-    {{ html()->modelForm($encoding, 'PUT', route('encodings.update', $encoding))->open() }}
-        @include('encodings.form')
-
-        <footer class="py-4 justify-end">
-            <button class="btn btn-blue">
-                Save Changes 
-            </button>
-        </footer>
-    {{ html()->closeModelForm() }}
-</section>
-
 
 <form action="{{ route('encoding.metas.store', $encoding) }}" method="post"
-    class="mt-8 bg-gray-300 p-4">
+    class="mt-8 bg-gray-300 p-4 mb-4">
     @csrf
 
     <h1 class="m-2 font-semibold">
@@ -41,6 +29,19 @@
         Add this tag to encoding {{ $encoding->encoder_assigned_id }}
     </button>
 </form>
+
+
+<section class="flex flex-wrap mb-8 p-4 border border-1 border-gray-600">
+    {{ html()->modelForm($encoding, 'PUT', route('encodings.update', $encoding))->open() }}
+        @include('encodings.form')
+
+        <footer class="py-4 justify-end">
+            <button class="btn btn-blue">
+                Save Changes 
+            </button>
+        </footer>
+    {{ html()->closeModelForm() }}
+</section>
 
 <section class="flex flex-wrap">
     <h1 class="m-2 text-2xl w-full">
