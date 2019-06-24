82c76a7098bcc981c75e6ff19455404cacbeae32
chimalsky <ich.bin.abe@gmail.com>
2019-06-24 08:43:35 -0400

    value larger

--- resources/views/encodings/meta/item.blade.php
+++ resources/views/encodings/meta/item.blade.php
@@ -1,9 +1,9 @@
-<label class="w-1/2 px-2">
+<label class="w-1/5 px-2">
     <span class="text-gray-700 font-hairline">Tag Name</span>
     {{ html()->text('key')->class(['form-input', 'mt-1', 'block', 'w-full', 'font-medium']) }}
 </label>
 
-<label class="w-1/2 px-2">
+<label class="w-4/5 px-2">
     <span class="text-gray-700 font-hairline">Tag Value</span>
     {{ html()->text('value')->class(['form-input', 'mt-1', 'block', 'w-full', 'font-medium']) }}
 </label>
