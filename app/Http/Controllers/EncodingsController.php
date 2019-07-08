bf2ee3686cd154596808fdaddfd97200b778640e
chimalsky <ich.bin.abe@gmail.com>
2019-07-08 13:48:48 -0400

    all bug

--- app/Http/Controllers/EncodingsController.php
+++ app/Http/Controllers/EncodingsController.php
@@ -14,7 +14,7 @@ class EncodingsController extends Controller
      */
     public function index()
     {
-        $encodings = Encoding::with('resources')->all();
+        $encodings = Encoding::with('resources')->get();
         return view('encodings.index', compact('encodings'));
     }
 
