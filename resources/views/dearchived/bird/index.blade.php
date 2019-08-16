b692786e8d4d00c77b56bf851d5883452ea8f873
chimalsky <ich.bin.abe@gmail.com>
2019-08-16 12:38:29 -0400

    media organized

--- resources/views/dearchived/bird/index.blade.php
+++ resources/views/dearchived/bird/index.blade.php
@@ -3,30 +3,30 @@
 @section('content')
 
 @foreach($birds as $bird)
-    <article class="p-3 w-full md:w-1/2">
-        {{ $bird->name }}
-
-        <section class="p-2 text-gray-200 flex flex-wrap">
+    <article class="p-3 w-full md:w-1/2 text-gray-200">
+        <section class="p-2 flex flex-wrap">
             <section data-controller="frame" class="max-h-screen"
                 data-target="frame.container" >
-                @foreach ($bird->resources as $poem)
-                    @foreach ($bird->getMedia('', function($medium) { return Str::contains($medium->mime_type, 'audio'); }) as $audio)
+                    {{ $bird->name }}
+
+                    
+                    @if ($audio = $bird->getMedia('', function($medium) { return Str::contains($medium->mime_type, 'audio'); })->first() ?? false)
                         <audio controls
                         src="{{ $audio->getUrl() }}">
                             Your browser does not support the
                             <code>audio</code> element.
                         </audio>
+                    @endif
+                    @foreach ($bird->resources as $poem)
+                        @foreach ($poem->getMedia('', function($medium) { return Str::contains($medium->mime_type, 'image'); }) as $image)
+                                <div class="rounded-full cursor-move">
+                                    <img src="{{ $image->getUrl() }}" 
+                                    class="w-full object-none"
+                                    />
+                                </div>
+                        @endforeach
                     @endforeach
-                    @foreach ($poem->getMedia('', function($medium) { return Str::contains($medium->mime_type, 'image'); }) as $image)
-                            <div class="rounded-full cursor-move">
-                                <img src="{{ $image->getUrl() }}" 
-                                class="w-full object-none"
-                                />
-                            </div>
-                    @endforeach
-                @endforeach
             </section>
-
         </section>
     </article>
 @endforeach
