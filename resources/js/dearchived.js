dde89c4d89f5179551cbeb3ede63a7a2dcb747a3
Zdziarska <zdziarska@zdziarskas-imac.attlocal.net>
2019-10-02 15:53:35 -0400

    bird animation

--- resources/js/dearchived.js
+++ resources/js/dearchived.js
@@ -13,3 +13,27 @@ import 'jquery-ui/ui/widgets/draggable'
 const application = new Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+let birds = document.querySelectorAll('.bird')
+
+$(document).ready(function(){
+    birds.forEach((el) => {
+        let newq = makeNewPosition();
+
+        $(el).animate({ top: newq[0], left: newq[1] }, 5000, function(){
+              
+        });
+    })
+});
+
+function makeNewPosition(){
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
