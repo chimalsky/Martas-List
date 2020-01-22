6cd6106cecbdfb3e4ac3dd45f26c17bbee86a551
Zdziarska <zdziarska@zdziarskas-imac.attlocal.net>
2020-01-21 21:44:31 -0500

    scheduled backups.. giving it a try

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        $schedule->command('backup:run --only-db')
+            ->hourly();
     }
 
     /**
