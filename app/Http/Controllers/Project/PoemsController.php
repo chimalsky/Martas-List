f53c353806179bff70baaa9c0e7bdd1a3eba71a2
Zdziarska <zdziarska@zdziarskas-imac.attlocal.net>
2020-03-19 18:34:09 -0400

    sort poems by date

--- app/Http/Controllers/Project/PoemsController.php
+++ app/Http/Controllers/Project/PoemsController.php
@@ -10,9 +10,19 @@ class PoemsController extends Controller
 { 
     public function index(Request $request)
     {
-        $poems = Resource::with(['meta', 'connections'])
-            ->where('resource_type_id', 3)
-            ->get();
+        $poems = Resource::with(['meta', 'media'])
+            ->where('resource_type_id', 3);
+
+
+        $poems = $poems->addSelect(['queries_meta_value' => function($query)  {
+            $query->select('value')
+                ->from('resource_metas')
+                ->whereColumn('resource_id', 'resources.id')
+                ->where('resource_attribute_id', 131)
+                ->latest()->limit(1);
+        }])
+        ->orderBy('queries_meta_value', 'asc')
+        ->get();
 
         return view('project.poems.index', compact('poems'));
     }
