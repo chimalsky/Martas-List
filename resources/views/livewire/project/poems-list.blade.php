e041573caddc933ba77f633e5e91eb4e83940955
Abraham Kim <gucci@trap.attlocal.net>
2020-08-26 12:13:03 -0400

    animation doesn't work on filter-update

--- resources/views/livewire/project/poems-list.blade.php
+++ resources/views/livewire/project/poems-list.blade.php
@@ -1,8 +1,8 @@
 <div class="w-full">
-    <section style="animation-duration: 1s;
+    <section style="animation-duration: 900ms;
                 animation-name: slidein;">
         @if ($poems->count())
-            <main class="flex flex-wrap w-full pt-12" ">
+            <main class="flex flex-wrap w-full pt-12">
                 @foreach ($poems as $poem)
                     <article class="pb-32 px-4 w-full lg:w-1/2 xl:w-1/3">
                         @include('project.poems._single', ['poem' => $poem])
@@ -20,12 +20,16 @@
         @keyframes slidein {
             from {
                 margin-top: 100%;
+                margin-left: 100%;
                 height: 300%; 
+                width: 100%;
             }
 
             to {
                 margin-top: 0%;
+                margin-left: 0%;
                 height: 100%;
+                width: 100%;
             }
         }
     </style>
