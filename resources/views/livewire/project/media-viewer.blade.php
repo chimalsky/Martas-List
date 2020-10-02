709eaa2adf2d8ce0445d8e265bf1499df59b6c7b
Abraham Kim <gucci@trap.lan>
2020-10-02 15:30:04 -0400

    manuscript sizing

--- resources/views/livewire/project/media-viewer.blade.php
+++ resources/views/livewire/project/media-viewer.blade.php
@@ -32,7 +32,7 @@
                 <div wire:loading class="animate-ping h-12 w-12 text-gray-700 hover:text-gray-500 focus:outline-none 
                     focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 
                     transition ease-in-out duration-150">
-                    <img src="{{ asset('img/bird-icon-round.png') }}" />
+                    <img class="object-contain h-screen" src="{{ asset('img/bird-icon-round.png') }}" />
                 </div>
             </div>
         </main>
