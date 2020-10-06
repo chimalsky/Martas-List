fed43caa8569da77b2f5ea9c151dd21de1755322
Abraham Kim <gucci@trap.lan>
2020-10-05 21:02:41 -0400

    about dd

--- app/Http/Controllers/Project/AboutController.php
+++ app/Http/Controllers/Project/AboutController.php
@@ -10,6 +10,7 @@ class AboutController extends Controller
 {
     public function __invoke(Request $request)
     {
+        dd('about');
         $content = optional(ResourceMeta::find(42070))->value ?? 'No Content Yet';
 
         return view('project.about', compact('content'));
