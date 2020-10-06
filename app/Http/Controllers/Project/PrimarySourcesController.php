c4b48507f368df6d04f9ad5379fa2d82a49cea5e
Abraham Kim <gucci@trap.lan>
2020-10-05 21:21:55 -0400

    primary source meta id

--- app/Http/Controllers/Project/PrimarySourcesController.php
+++ app/Http/Controllers/Project/PrimarySourcesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Project;
 
+use App\ResourceMeta;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,7 @@ class PrimarySourcesController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $content = file_get_contents(base_path('/project/content/primary-sources.md'));
+        $content = optional(ResourceMeta::find(42071))->value ?? 'No Content Yet';
 
         return view('project.primary-sources', compact('content'));
     }
