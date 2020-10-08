09899cb1725f029728097915ee70994bdc790b95
Abraham Kim <gucci@trap.lan>
2020-10-08 13:09:44 -0400

    nav ui items

--- resources/views/project/_nav-items.blade.php
+++ resources/views/project/_nav-items.blade.php
@@ -2,13 +2,10 @@
     Home
 </a>
 <a href="@route('project.about')" class="block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
-    About
+    Coordinates
 </a>
 <a href="@route('project.primary-sources')" class="block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
-    Primary Sources
-</a>
-<a href="" class="opacity-50 block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
-    Coordinates
+    Sources
 </a>
 <a href="" class="opacity-50 block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
     Navigation
