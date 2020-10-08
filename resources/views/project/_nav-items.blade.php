6456c501a516b32d2c484208451feb409518f6de
Abraham Kim <gucci@trap.lan>
2020-10-08 13:10:30 -0400

    nav

--- resources/views/project/_nav-items.blade.php
+++ resources/views/project/_nav-items.blade.php
@@ -22,7 +22,7 @@
 <a href="@route('project.poems.index')" class="opacity-50 font-bold block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
     Cultural Archive
 </a>
-<a href="" class="block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
+<a href="" class="opacity-50 block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
     Visitors' Field Notes
 </a>
 <a href="" class="opacity-50 block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
@@ -31,6 +31,6 @@
 <a href="" class="opacity-50 block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
     Glossary
 </a>
-<a href="@route('project.poems.index')" class="opacity-50 block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
+<a href="" class="opacity-50 block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
     Colophon
 </a>
