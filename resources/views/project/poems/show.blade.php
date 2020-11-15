2cd0271f528d68183b03c62fb982c9d18aa963f3
Abraham Kim <gucci@trap.attlocal.net>
2020-11-15 15:40:50 -0500

    styling

--- resources/views/project/poems/show.blade.php
+++ resources/views/project/poems/show.blade.php
@@ -56,7 +56,7 @@
     
     <main class="flex flex-wrap justify-center">
         @foreach ($birds as $bird)
-            <article class="bird shadow-lg pt-2 pb-6 px-4 w-full lg:w-1/2 xl:w-1/3">
+            <article class="bird pt-2 pb-6 px-4 w-full lg:w-1/2 xl:w-1/3">
                 @include('project.birds._single', $bird)
             </article>
         @endforeach
