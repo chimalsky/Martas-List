bf5041731d1ae6ede97518dba3f98f523b526ce6
Abraham Kim <ich.bin.abe@gmail.com>
2021-05-27 14:09:48 -0400

    blocks need different option display

--- resources/views/components/project/filter/panel.blade.php
+++ resources/views/components/project/filter/panel.blade.php
@@ -1,12 +1,37 @@
 <div>
     @if ($filterable->id == 131)
         <x-project.filter.year :filterable="$filterable" />
+    @elseif ($filterable->id == 623)
+        @foreach ($filterable->nonNullOptions as $option)
+            <label class="block cursor-pointer">
+                <input data-action="change->form#changed" type="checkbox"
+                    name="filterable[{{ $filterable->id }}][]"
+                    value="{{ $option['_name'] }}"
+                    class=""
+                    @if (collect(request()->input('filterable.' . $filterable->id))->contains($option['_name']))
+                        checked 
+                    @endif
+                    autocomplete="off" 
+                        />
+                <span class="pl-2">
+                    {{ $option['_name'] }}
+                </span>
+            </label>
+        @endforeach
     @else 
         @foreach ($filterable->nonNullOptions as $option)
             <label class="block cursor-pointer">
-               
+                <input data-action="change->form#changed" type="checkbox"
+                    name="filterable[{{ $filterable->id }}][]"
+                    value="{{ $option }}"
+                    class=""
+                    @if (collect(request()->input('filterable.' . $filterable->id))->contains($option))
+                        checked 
+                    @endif
+                    autocomplete="off" 
+                        />
                 <span class="pl-2">
-                    {{ gettype($option) }}
+                    {{ $option }}
                 </span>
             </label>
         @endforeach
