518f1347841a5b8972579b05446f4ce4267f3d6b
Abraham Kim <ich.bin.abe@gmail.com>
2021-05-27 13:56:03 -0400

    debug

--- resources/views/components/project/filter/panel.blade.php
+++ resources/views/components/project/filter/panel.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{ $filterable }}
+    @if ($filterable->id == 131)
+        <x-project.filter.year :filterable="$filterable" />
+    @else 
+        @foreach ($filterable->nonNullOptions as $option)
+            <label class="block cursor-pointer">
+               
+                <span class="pl-2">
+                    {{ gettype($option) }}
+                </span>
+            </label>
+        @endforeach
+    @endif
 </div>
