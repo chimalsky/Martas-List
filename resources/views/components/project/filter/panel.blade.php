ce4a62d4bf581d338ebd55461a2d77798ea01bc2
Abraham Kim <ich.bin.abe@gmail.com>
2021-05-27 13:51:47 -0400

    test

--- resources/views/components/project/filter/panel.blade.php
+++ resources/views/components/project/filter/panel.blade.php
@@ -1,24 +1,3 @@
 <div>
-    @if ($filterable->id == 131)
-        <x-project.filter.year :filterable="$filterable" />
-    @elseif ($filterable->id == 138)
-        foo
-    @else 
-        @foreach ($filterable->nonNullOptions as $option)
-            <label class="block cursor-pointer">
-                <input data-action="change->form#changed" type="checkbox"
-                    name="filterable[{{ $filterable->id }}][]"
-                    value="{{ $option }}"
-                    class=""
-                    @if (collect(request()->input('filterable.' . $filterable->id))->contains($option))
-                        checked 
-                    @endif
-                    autocomplete="off" 
-                        />
-                <span class="pl-2">
-                    {{ $option }}
-                </span>
-            </label>
-        @endforeach
-    @endif
+    {{ $filterable }}
 </div>
