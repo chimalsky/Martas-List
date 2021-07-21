ff980439332d520d9967a82405b94d989d130c50
Abraham Kim <ich.bin.abe@gmail.com>
2021-07-21 14:26:35 -0400

    fix content ids

--- resources/views/project/digital-objects/map.blade.php
+++ resources/views/project/digital-objects/map.blade.php
@@ -2,6 +2,6 @@
 
 @section ('content')
 <main class="max-w-2xl mx-auto text-gray-700 text-lg page-content" data-style="red">
-    {!! optional(App\ResourceMeta::find(42072))->value ?? 'No content yet' !!}
+    {!! optional(App\ResourceMeta::find(46378))->value ?? 'No content yet' !!}
 </main>
 @endsection
