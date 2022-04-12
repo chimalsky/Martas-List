df9282e8af89ca982c0ddc94a405ec1a3ae99c18
Abraham Kim <ich.bin.abe@gmail.com>
2022-04-12 11:49:43 -0400

    Trying out soundcloud embed

--- resources/views/project/digital-objects/birdring.blade.php
+++ resources/views/project/digital-objects/birdring.blade.php
@@ -26,6 +26,9 @@
             Pause
         </button>
     </div>
+    <div class="mx-auto max-w-sm">
+        <iframe width="100%" height="120" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1229912158&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/danielle-richards-973620677" title="Danielle Richards" target="_blank" style="color: #cccccc; text-decoration: none;">Danielle Richards</a> · <a href="https://soundcloud.com/danielle-richards-973620677/july" title="July" target="_blank" style="color: #cccccc; text-decoration: none;">July</a></div>
+    </div>
     <section class="bg-yellow-300 p-24 mt-20">
         Notes
     </section>
