28885dc1935aa72fa2ec2b669b9052b166c20b4d
Abraham Kim <ich.bin.abe@gmail.com>
2023-03-16 23:20:59 -0400

    circulation

--- resources/views/project/poems/_circulation.blade.php
+++ resources/views/project/poems/_circulation.blade.php
@@ -1,21 +1,22 @@
 @if ($poem->circulation && $poem->circulation->value == 'unknown')
     <span class="italic">Circulation status unknown.</span>
 @else
-    @if ($poem->formOfSentPoem)
-        @if (strtolower($poem->formOfSentPoem->value) == 'poem-message')
-            <span class="italic">Poem-message</span>
-        @endif
-        @if (strtolower($poem->formOfSentPoem->value) == 'poem')
-            <span class="italic">Poem</span>
-        @endif
-        @if (strtolower($poem->formOfSentPoem->value) == 'poem embedded in a message')
-            <span class="italic">Poem embedded in a message</span>
-        @endif
-        @if (strtolower($poem->formOfSentPoem->value) == 'poem enclosed in a message')
-            <span class="italic">Poem enclosed in a message</span>
-        @endif
-        @if (strtolower($poem->formOfSentPoem->value) == 'poem embedded in a letter draft (uncirculated)')
-            <span class="italic">Poem embedded in a letter draft (uncirculated)</span>
+    @if (strtolower($poem->formOfSentPoem->value) == 'poem embedded in a letter draft (uncirculated)')
+        <span class="italic">Retained in Dickinson's archive.</span>
+    @else
+        @if ($poem->formOfSentPoem)
+            @if (strtolower($poem->formOfSentPoem->value) == 'poem-message')
+                <span class="italic">Poem-message</span>
+            @endif
+            @if (strtolower($poem->formOfSentPoem->value) == 'poem')
+                <span class="italic">Poem</span>
+            @endif
+            @if (strtolower($poem->formOfSentPoem->value) == 'poem embedded in a message')
+                <span class="italic">Poem embedded in a message</span>
+            @endif
+            @if (strtolower($poem->formOfSentPoem->value) == 'poem enclosed in a message')
+                <span class="italic">Poem enclosed in a message</span>
+            @endif
         @endif
 
         Sent to ({{ $poem->circulation->value }}).
