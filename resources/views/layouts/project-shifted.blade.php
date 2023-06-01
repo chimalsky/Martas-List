44f607c82a01947d6cb676b2c5dc6dedae2b7632
Abraham Kim <ich.bin.abe@gmail.com>
2023-06-01 08:57:28 -0400

    google font import

--- resources/views/layouts/project-shifted.blade.php
+++ resources/views/layouts/project-shifted.blade.php
@@ -26,7 +26,7 @@
         
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400;1,500&family=Cormorant+SC&family=Cormorant+Upright:wght@300;400;500;600;700&display=swap" rel="stylesheet"> 
-        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=Cormorant:ital,wght@0,300;0,400;0,600;0,700;1,400&display=swap&family=IM+Fell+Double+Pica&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=Cormorant:ital,wght@0,300;0,400;0,600;0,700;1,400&family=IM+Fell+Double+Pica&family=Cormorant+Unicase&display=swap" rel="stylesheet">
 
         @stack('scripts')
     </head>
