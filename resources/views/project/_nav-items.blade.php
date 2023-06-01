0d5e972a8a4ab435e6ae6e98bf9080ec49b151d2
Abraham Kim <ich.bin.abe@gmail.com>
2023-06-01 11:11:56 -0400

    change naming of bird-ring to sound-ring
    embed figma iframe

--- resources/views/project/_nav-items.blade.php
+++ resources/views/project/_nav-items.blade.php
@@ -19,7 +19,7 @@
 </a>
 <a href="@route('project.digital-objects.birdring')" class="font-bold block px-4 py-2 leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900">
     <img src="{{ asset('img/do-3.png') }}" class="w-8 rounded-full object-cover inline-block mr-2" /> 
-    <span style='margin-left: 2px'>Bird-Ring</span>
+    <span style='margin-left: 2px'>Sound-Ring</span>
 </a>
 <a href="" style="background-color: #F7F5E7; color: #7f7f7f;" class="font-bold block px-4 py-2 leading-5 cursor-text">
     Visitors' Field Notes
--- resources/views/project/digital-objects/birdring.blade.php
+++ resources/views/project/digital-objects/birdring.blade.php
@@ -18,9 +18,13 @@
 @endsection
 
 @section('content')
+<div class="mt-10">
+    <iframe style="border: 1px solid rgba(0, 0, 0, 0.1); margin: 0 auto;" width="92%" height="720" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FtzwBR4UCuQ0dNjtjgL2U0L%2FSound-Ring-%257C-Prototype-SP23%3Ftype%3Ddesign%26node-id%3D0%253A1%26t%3D6fIDr0vUH79fjL68-1" allowfullscreen></iframe>
+</div>
+
 <main id="birdring-wrapper" 
     class="max-w-5xl mx-auto text-gray-700 text-lg page-content" 
-    style="margin-top: -105px;" 
+    style="margin-top: 0;" 
     data-style="red">
 </main>
 @endsection
